3bb4d216943cf8a1801a062300fc4258fa396ff9
FabienC <fabiencoutant.ctt@gmail.com>
2025-05-23 16:59:11 +0300

    fix: issue_09 - accrueInterestToFeeRecipient attempts to transfer an incorrect amount of tokens

--- contracts/flashloan/FlashParallelToken.sol
+++ contracts/flashloan/FlashParallelToken.sol
@@ -134,11 +134,13 @@ contract FlashParallelToken is
 
     /// @notice Accrues interest to the fee recipient for a given list of tokens
     /// @param tokens List of addresses of tokens to accrue interest for
-    /// @return balance Amount of interest accrued
-    function accrueInterestToFeeRecipient(address[] calldata tokens) external returns (uint256 balance) {
+    /// @return balances Amounts of interest accrued
+    function accrueInterestToFeeRecipient(address[] calldata tokens) external returns (uint256[] memory balances) {
+        balances = new uint256[](tokens.length);
         for (uint256 i = 0; i < tokens.length; i++) {
             IERC20 token = IERC20(tokens[i]);
-            balance += token.balanceOf(address(this));
+            uint256 balance= token.balanceOf(address(this));
+            balances[i]=balance;
             token.safeTransfer(flashLoanFeeRecipient, balance);
         }
     }
